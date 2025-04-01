15b24fdbddcf9d363adf0089ff774078461dbd17
abhay <abhayrawatar28@gmail.com>
2025-04-02 00:02:11 +0530

    Disable specific ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "next/next/no-img-element": "off", 
+      "prefer-const": "off", 
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-html-link-for-pages": "off", 
+    },
+  },
 ];
 
 export default eslintConfig;
